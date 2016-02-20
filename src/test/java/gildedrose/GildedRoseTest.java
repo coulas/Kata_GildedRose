7e6253350db3ca51a421f3b2c8c3258998ad1c2a
Nicolas Fédou <nicolas.fedou@gmail.com>
2016-02-20 23:05:01 +0100

    I append tests to cover code one after the other, but come times, when to cover next condition, you see that you derive from a previous test. That just happened with negative sellin where I added,   one for default items,   one for Aged Brie and   a last for backstage... I added them after the existing test of the same items.
    Let's make more sense of those tests as they are named upon the data entry and not with the functionality they trigger !

--- src/test/java/gildedrose/GildedRoseTest.java
+++ src/test/java/gildedrose/GildedRoseTest.java
@@ -26,9 +26,31 @@ public class GildedRoseTest {
     }
 
     @Test
+    public void updateQuality_for_default_items_with_negative_sellin() {
+        Item[] givenDefaultItems = new Item[]{new Item("default", -1, 20)};
+        Item[] expectedDefaultItems = new Item[]{new Item("default", -2, 18)};
+        GildedRose gildedRose = new GildedRose(givenDefaultItems);
+        gildedRose.updateQuality();
+        assertThat(gildedRose.getItems())
+                .usingFieldByFieldElementComparator()
+                .containsExactly(expectedDefaultItems);
+    }
+
+    @Test
     public void updateQuality_for_Aged_Brie() {
         Item[] givenAnAgedBrie = new Item[]{new Item("Aged Brie", 30, 20)};
         Item[] expectedDefaultItems = new Item[]{new Item("Aged Brie", 29, 21)};
+        GildedRose gildedRose = new GildedRose(givenAnAgedBrie);
+        gildedRose.updateQuality();
+        assertThat(gildedRose.getItems())
+                .usingFieldByFieldElementComparator()
+                .containsExactly(expectedDefaultItems);
+    }
+
+    @Test
+    public void updateQuality_for_Aged_Brie_with_negative_sellin() {
+        Item[] givenAnAgedBrie = new Item[]{new Item("Aged Brie", -1, 20)};
+        Item[] expectedDefaultItems = new Item[]{new Item("Aged Brie", -2, 22)};
         GildedRose gildedRose = new GildedRose(givenAnAgedBrie);
         gildedRose.updateQuality();
         assertThat(gildedRose.getItems())
@@ -61,6 +83,16 @@ public class GildedRoseTest {
     public void updateQuality_for_Backstage_with_very_low_sellin() {
         Item[] givenAnAgedBrie = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 5, 20)};
         Item[] expectedDefaultItems = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 4, 23)};
+        GildedRose gildedRose = new GildedRose(givenAnAgedBrie);
+        gildedRose.updateQuality();
+        assertThat(gildedRose.getItems())
+                .usingFieldByFieldElementComparator()
+                .containsExactly(expectedDefaultItems);
+    }
+    @Test
+    public void updateQuality_for_Backstage_with_negative_sellin() {
+        Item[] givenAnAgedBrie = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", -1, 20)};
+        Item[] expectedDefaultItems = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", -2, 0)};
         GildedRose gildedRose = new GildedRose(givenAnAgedBrie);
         gildedRose.updateQuality();
         assertThat(gildedRose.getItems())
