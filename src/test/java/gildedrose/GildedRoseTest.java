e46f6d663be451af25f934485edcda74f42f3187
Nicolas Fédou <nicolas.fedou@gmail.com>
2016-02-20 22:24:01 +0100

    added a first unit test that does not work as equals is not implemented, and we use deprecated assertions

--- src/test/java/gildedrose/GildedRoseTest.java
+++ src/test/java/gildedrose/GildedRoseTest.java
@@ -1,15 +1,23 @@
 package gildedrose;
 
+import static java.util.Arrays.asList;
 import static org.junit.Assert.*;
 
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 
 public class GildedRoseTest {
 
 	@Test
 	public void testTheTruth() {
-		Assert.assertTrue(true);
+		Item[] givenDefaultItems = new Item[]{new Item("default", 30, 20)};
+		Item[] expectedDefaultItems = new Item[]{new Item("default", 29, 19)};
+		GildedRose gildedRose = new GildedRose(givenDefaultItems);
+		gildedRose.updateQuality();
+		Assert.assertEquals(gildedRose.getItems(), expectedDefaultItems);
 	}
 }
