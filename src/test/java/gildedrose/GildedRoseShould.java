e02408154a690e66b1cb2eecf5f13dde2859fd4f
Nicolas Fédou <nicolas.fedou@gmail.com>
2016-02-20 23:24:10 +0100

    Now that we spoke with analysts of our system, our data driven tests looks pretty much like "behavior testing" and behavior is more durable than implementation. Our tests are more readable, more maintainable, less prone to updates, in short : more valuable !

--- src/test/java/gildedrose/GildedRoseShould.java
+++ src/test/java/gildedrose/GildedRoseShould.java
@@ -1,21 +1,14 @@
 package gildedrose;
 
-import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.*;
 
-import org.assertj.core.api.Assertions;
-import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.List;
 
-
-public class GildedRoseTest {
+public class GildedRoseShould {
 
     @Test
-    public void updateQuality_for_default_items() {
+    public void decrease_quality_and_sellin_of_default_items() {
         Item[] givenDefaultItems = new Item[]{new Item("default", 30, 20)};
         Item[] expectedDefaultItems = new Item[]{new Item("default", 29, 19)};
         GildedRose gildedRose = new GildedRose(givenDefaultItems);
@@ -26,7 +19,7 @@ public class GildedRoseTest {
     }
 
     @Test
-    public void updateQuality_for_default_items_with_negative_sellin() {
+    public void decrease_quality_twice_as_fast_of_default_items_when_sellin_is_negative() {
         Item[] givenDefaultItems = new Item[]{new Item("default", -1, 20)};
         Item[] expectedDefaultItems = new Item[]{new Item("default", -2, 18)};
         GildedRose gildedRose = new GildedRose(givenDefaultItems);
@@ -37,7 +30,7 @@ public class GildedRoseTest {
     }
 
     @Test
-    public void updateQuality_for_Sulfuras() {
+    public void not_modify_sulfuras() {
         Item[] givenDefaultItems = new Item[]{new Item("Sulfuras, Hand of Ragnaros", 30, 20)};
         Item[] expectedDefaultItems = new Item[]{new Item("Sulfuras, Hand of Ragnaros", 30, 20)};
         GildedRose gildedRose = new GildedRose(givenDefaultItems);
@@ -48,7 +41,7 @@ public class GildedRoseTest {
     }
 
     @Test
-    public void updateQuality_for_Sulfuras_with_negative_sellin() {
+    public void not_modify_sulfuras_even_with_negative_sellin() {
         Item[] givenDefaultItems = new Item[]{new Item("Sulfuras, Hand of Ragnaros", -1, 20)};
         Item[] expectedDefaultItems = new Item[]{new Item("Sulfuras, Hand of Ragnaros", -1, 20)};
         GildedRose gildedRose = new GildedRose(givenDefaultItems);
@@ -59,7 +52,7 @@ public class GildedRoseTest {
     }
 
     @Test
-    public void updateQuality_for_Aged_Brie() {
+    public void increase_quality_of_aged_brie() {
         Item[] givenAnAgedBrie = new Item[]{new Item("Aged Brie", 30, 20)};
         Item[] expectedDefaultItems = new Item[]{new Item("Aged Brie", 29, 21)};
         GildedRose gildedRose = new GildedRose(givenAnAgedBrie);
@@ -70,7 +63,7 @@ public class GildedRoseTest {
     }
 
     @Test
-    public void updateQuality_for_Aged_Brie_with_negative_sellin() {
+    public void increase_quality_twice_as_fast_of_aged_brie_when_sellin_is_low() {
         Item[] givenAnAgedBrie = new Item[]{new Item("Aged Brie", -1, 20)};
         Item[] expectedDefaultItems = new Item[]{new Item("Aged Brie", -2, 22)};
         GildedRose gildedRose = new GildedRose(givenAnAgedBrie);
@@ -81,7 +74,7 @@ public class GildedRoseTest {
     }
 
     @Test
-    public void updateQuality_for_Backstage() {
+    public void increase_quality_of_backstage() {
         Item[] givenAnAgedBrie = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 30, 20)};
         Item[] expectedDefaultItems = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 29, 21)};
         GildedRose gildedRose = new GildedRose(givenAnAgedBrie);
@@ -92,7 +85,7 @@ public class GildedRoseTest {
     }
 
     @Test
-    public void updateQuality_for_Backstage_with_low_sellin() {
+    public void increase_quality_twice_as_fast_of_backstage_when_sellin_is_low() {
         Item[] givenAnAgedBrie = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 10, 20)};
         Item[] expectedDefaultItems = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 9, 22)};
         GildedRose gildedRose = new GildedRose(givenAnAgedBrie);
@@ -102,7 +95,7 @@ public class GildedRoseTest {
                 .containsExactly(expectedDefaultItems);
     }
     @Test
-    public void updateQuality_for_Backstage_with_very_low_sellin() {
+    public void increase_quality_three_times_as_fast_of_backstage_when_sellin_is_very_low() {
         Item[] givenAnAgedBrie = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 5, 20)};
         Item[] expectedDefaultItems = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 4, 23)};
         GildedRose gildedRose = new GildedRose(givenAnAgedBrie);
@@ -112,7 +105,7 @@ public class GildedRoseTest {
                 .containsExactly(expectedDefaultItems);
     }
     @Test
-    public void updateQuality_for_Backstage_with_negative_sellin() {
+    public void delete_quality_of_backstage_when_sellin_is_negative() {
         Item[] givenAnAgedBrie = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", -1, 20)};
         Item[] expectedDefaultItems = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", -2, 0)};
         GildedRose gildedRose = new GildedRose(givenAnAgedBrie);
