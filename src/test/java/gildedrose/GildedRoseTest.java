0b4914514347e17c0973678595d9e52ae863bc94
Nicolas Fédou <nicolas.fedou@gmail.com>
2016-02-20 22:57:43 +0100

    adding tests one after the other. Several ways exists, some create tests with data, calls the method, without asserting to see where it passes with coverage and covers the whole code fast. Then they look for meaning by asserting and keep the meaning in the name of test. Most people prefer to make a passing test, therefore knowing why it passes and defining e test name, before moving to the next test.

--- src/test/java/gildedrose/GildedRoseTest.java
+++ src/test/java/gildedrose/GildedRoseTest.java
@@ -15,10 +15,53 @@ import java.util.List;
 public class GildedRoseTest {
 
     @Test
-    public void testTheTruth() {
+    public void updateQuality_for_default_items() {
         Item[] givenDefaultItems = new Item[]{new Item("default", 30, 20)};
         Item[] expectedDefaultItems = new Item[]{new Item("default", 29, 19)};
         GildedRose gildedRose = new GildedRose(givenDefaultItems);
+        gildedRose.updateQuality();
+        assertThat(gildedRose.getItems())
+                .usingFieldByFieldElementComparator()
+                .containsExactly(expectedDefaultItems);
+    }
+
+    @Test
+    public void updateQuality_for_Aged_Brie() {
+        Item[] givenAnAgedBrie = new Item[]{new Item("Aged Brie", 30, 20)};
+        Item[] expectedDefaultItems = new Item[]{new Item("Aged Brie", 29, 21)};
+        GildedRose gildedRose = new GildedRose(givenAnAgedBrie);
+        gildedRose.updateQuality();
+        assertThat(gildedRose.getItems())
+                .usingFieldByFieldElementComparator()
+                .containsExactly(expectedDefaultItems);
+    }
+
+    @Test
+    public void updateQuality_for_Backstage() {
+        Item[] givenAnAgedBrie = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 30, 20)};
+        Item[] expectedDefaultItems = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 29, 21)};
+        GildedRose gildedRose = new GildedRose(givenAnAgedBrie);
+        gildedRose.updateQuality();
+        assertThat(gildedRose.getItems())
+                .usingFieldByFieldElementComparator()
+                .containsExactly(expectedDefaultItems);
+    }
+
+    @Test
+    public void updateQuality_for_Backstage_with_low_sellin() {
+        Item[] givenAnAgedBrie = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 10, 20)};
+        Item[] expectedDefaultItems = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 9, 22)};
+        GildedRose gildedRose = new GildedRose(givenAnAgedBrie);
+        gildedRose.updateQuality();
+        assertThat(gildedRose.getItems())
+                .usingFieldByFieldElementComparator()
+                .containsExactly(expectedDefaultItems);
+    }
+    @Test
+    public void updateQuality_for_Backstage_with_very_low_sellin() {
+        Item[] givenAnAgedBrie = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 5, 20)};
+        Item[] expectedDefaultItems = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 4, 23)};
+        GildedRose gildedRose = new GildedRose(givenAnAgedBrie);
         gildedRose.updateQuality();
         assertThat(gildedRose.getItems())
                 .usingFieldByFieldElementComparator()
