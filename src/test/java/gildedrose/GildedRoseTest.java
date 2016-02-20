7c4f2550dc48c1cb0fa3f51fcc364c932d96e27e
Nicolas Fédou <nicolas.fedou@gmail.com>
2016-02-20 22:38:49 +0100

    assertJ helps testing JavaBeans, Pojos and other objects with fields we can "get". It also can help giving insight in your collections. Here, we use "usingFieldByFieldElementComparator" to avoid adding equal to Item.

--- src/test/java/gildedrose/GildedRoseTest.java
+++ src/test/java/gildedrose/GildedRoseTest.java
@@ -1,8 +1,10 @@
 package gildedrose;
 
 import static java.util.Arrays.asList;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.*;
 
+import org.assertj.core.api.Assertions;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -12,12 +14,14 @@ import java.util.List;
 
 public class GildedRoseTest {
 
-	@Test
-	public void testTheTruth() {
-		Item[] givenDefaultItems = new Item[]{new Item("default", 30, 20)};
-		Item[] expectedDefaultItems = new Item[]{new Item("default", 29, 19)};
-		GildedRose gildedRose = new GildedRose(givenDefaultItems);
-		gildedRose.updateQuality();
-		Assert.assertEquals(gildedRose.getItems(), expectedDefaultItems);
-	}
+    @Test
+    public void testTheTruth() {
+        Item[] givenDefaultItems = new Item[]{new Item("default", 30, 20)};
+        Item[] expectedDefaultItems = new Item[]{new Item("default", 29, 19)};
+        GildedRose gildedRose = new GildedRose(givenDefaultItems);
+        gildedRose.updateQuality();
+        assertThat(gildedRose.getItems())
+                .usingFieldByFieldElementComparator()
+                .containsExactly(expectedDefaultItems);
+    }
 }
